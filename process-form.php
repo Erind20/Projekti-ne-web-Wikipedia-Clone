64e19aee3e671946e8aa82338395a353d3e762ad
Erind20 <118070513+Erind20@users.noreply.github.com>
2023-02-04 02:44:45 +0100

    Add files via upload

---
+++ process-form.php
@@ -0,0 +1,96 @@
+<?php
+
+$name = $_POST["name"];
+$message = $_POST["message"];
+$priority = filter_input(INPUT_POST, "priority", FILTER_VALIDATE_INT);
+$type = filter_input(INPUT_POST, "type", FILTER_VALIDATE_INT);
+$terms = filter_input(INPUT_POST, "terms", FILTER_VALIDATE_BOOL);
+
+if ( ! $terms) {
+    die("Kushtet nuk janë pranuar, ju lutemi kthehuni prapa !");    
+}   
+
+$host = "localhost";
+$dbname = "message_db";
+$username = "root";
+$password = "";
+        
+$conn = mysqli_connect(hostname: $host,
+                       username: $username,
+                       password: $password,
+                       database: $dbname);
+        
+if (mysqli_connect_errno()) {
+    die("Connection error: " . mysqli_connect_error());
+}           
+        
+$sql = "INSERT INTO message (name, body, priority, type)
+        VALUES (?, ?, ?, ?)";
+
+$stmt = mysqli_stmt_init($conn);
+
+if ( ! mysqli_stmt_prepare($stmt, $sql)) {
+ 
+    die(mysqli_error($conn));
+}
+
+mysqli_stmt_bind_param($stmt, "ssii",
+                       $name,
+                       $message,
+                       $priority,
+                       $type);
+
+mysqli_stmt_execute($stmt);
+?>
+
+<html>
+  <head>
+    <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,400i,700,900&display=swap" rel="stylesheet">
+  </head>
+    <style>
+      body {
+        text-align: center;
+        padding: 40px 0;
+        background: #EBF0F5;
+      }
+        h1 {
+          color: #88B04B;
+          font-family: "Nunito Sans", "Helvetica Neue", sans-serif;
+          font-weight: 900;
+          font-size: 40px;
+          margin-bottom: 10px;
+        }
+        p {
+          color: #404F5E;
+          font-family: "Nunito Sans", "Helvetica Neue", sans-serif;
+          font-size:20px;
+        }
+      i {
+        color: #9ABC66;
+        font-size: 100px;
+        line-height: 200px;
+        margin-left:-15px;
+      }
+      .card {
+        background: white;
+        padding: 60px;
+        border-radius: 4px;
+        box-shadow: 0 2px 3px #C8D0D8;
+        display: inline-block;
+        margin: 0 auto;
+      }
+      a{
+          margin-top:2rem;
+      }
+    </style>
+    <body>
+      <div class="card">
+      <div style="border-radius:200px; height:200px; width:200px; background: #F8FAF5; margin:0 auto;">
+        <i class="checkmark">✓</i>
+      </div>
+        <h1>Sukses 😃</h1> 
+        <p>Ne e pranuam mesazhin e juaj.<br/> Do të jemi në kontakt së shpejti !</p>
+        <a href="index.html">Kthehu prapa</a>
+      </div>
+    </body>
+</html>
